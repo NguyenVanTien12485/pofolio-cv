451613a4fa79b177e8dea393413bc2c9f8625cdd
tien nguyen <tiennguyen@okxe.vn>
2023-03-27 00:45:51 +0700

    update file css ui

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -1,71 +1,33 @@
 <template>
-    <section id="portfolio" class="w-full">
-      <div class="max-w-[1520px] mx-auto px-10">
-        <Carousel ref="carousel" v-model="currentSlide" snapAlign="start">
-            <Slide v-for="slide in 10" :key="slide">
-                <div class="carousel__item flex flex-row">
-                  <!-- left -->
-                  <div class="">
-                    <div>
-                      <span>Client</span>
-                      <img src="../assets/icons/🤝.svg" alt="">
-                      <img src="../assets/icons/den-xanh.svg" alt="">
-                    </div>
-  
-                    <div>
-                      <span>UI Showcase</span>
-                      <span>Web App Platform</span>
-                    </div>
-  
-                    <h3>Influencer Marketing</h3>
-  
-                    <p>
-                      Kazac là một nền tảng kết nối các thương hiệu cần quảng bá sản phẩm với những người ảnh hưởng có lượng theo dõi lớn. Ngoài việc kết nối thương hiệu và người ảnh hưởng, Kazac còn có các tính năng cho phép người dùng theo dõi hiệu quả của các bài đăng quảng cáo và quản lý tài khoản một cách nhanh chóng và thuận tiện. 
-                    </p>
-  
-                    <button>
-                      Xem chi tiet
-                      <img src="../assets/icons/arrow-right.svg" alt="">
-                    </button>
-  
-                    <div>
-                      <div>
-                        <img src="" alt="">
-                      </div>
-                      <span>
-                        Follow my dribbble.com/englu
-                      </span>
-                    </div>
-                  </div>
+  <section id="portfolio" class="w-full">
+    <div class="max-w-[1520px] mx-auto px-10">
+      <Carousel ref="carousel" v-model="currentSlide" >
+        <Slide v-for="slide in 10" :key="slide">
+          <div>{{ slide - 1 }}</div>
+        </Slide>
+      </Carousel>
+    </div>
 
-                  <!-- right -->
-                  <div class="">
-                    <div class="relative  w-full h-full">
-                      <img src="../assets/images/influencer-maketing/big.png" alt="" class="w-full h-full right-0 bottom-0 absolute">
-                    </div>
-                  </div>
-                </div>
-            </Slide>
-        </Carousel>
+    <!-- navigator and panigation -->
+    <div class="max-w-[1520px] mx-auto flex justify-between">
+      <div>
+        <button @click="prev">Prev</button>
+        <button @click="next">Next</button>
       </div>
-
-      <!-- navigator and panigation -->
-        <div class="max-w-[1520px] mx-auto flex justify-between">
-          <div>
-            <button @click="prev">Prev</button>
-            <button @click="next">Next</button>
-          </div>
-          <ol class="flex flex-row mb-10">
-            <li v-for="slide in 10" :key="slide" class="inline-block w-5 h-5 bg-red-500 mx-4" @click="handleClick(slide)"></li>
-          </ol>
-        </div>
-    </section>
-
+      <ol class="flex flex-row mb-10">
+        <li v-for="slide in 10" :key="slide" class="inline-block w-5 h-5 bg-red-500 mx-4" @click="handleClick(slide)">
+        </li>
+      </ol>
+    </div>
+  </section>
 </template>
   
-  <script>
+<script>
 import { defineComponent } from 'vue'
+import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Navigation, Slide, Pagination } from 'vue3-carousel'
+// import 'vue3-carousel/dist/carousel'
+// import 'vue3-carousel/carousel.css'
 export default defineComponent({
   name: 'CustomNavigation',
   components: {
@@ -78,14 +40,14 @@ export default defineComponent({
     currentSlide: 1,
   }),
   mounted() {
+    // console.dir(this.$refs.carousel)
     // console.log()
+    document.querySelector('.carousel__liveregion').style.display = 'none'
+
   },
   methods: {
-    demo() {
-      console.dir(this.$refs.ahihi);
-    },
     handleClick(index) {
-      this.$refs.carousel.slideTo(index)
+      this.$refs.carousel.slideTo(index - 1)
     },
     next() {
       this.$refs.carousel.next()
@@ -96,3 +58,5 @@ export default defineComponent({
   },
 })
 </script>
+<style scoped>
+</style>
